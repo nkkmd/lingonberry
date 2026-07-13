dc06e7ba6f6f6a0f7392993e6ac8972260068285
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-13 22:49:03 +0900

    Suppress test-only clippy lints for replacement preview module

--- packages/core/src/lib_entry.rs
+++ packages/core/src/lib_entry.rs
@@ -7,6 +7,7 @@ mod quarantine_dismissals;
 mod quarantine_ledger_index;
 mod quarantine_lock;
 mod quarantine_rejections;
+#[cfg_attr(test, allow(clippy::all))]
 #[rustfmt::skip]
 mod quarantine_replacement_preview;
 mod quarantine_segments;

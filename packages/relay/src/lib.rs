adf8a4b870d37f9519a894ce2a9ccc2b94f9ba55
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-19 10:53:36 +0900

    apply rustfmt to relay contract exports

--- packages/relay/src/lib.rs
+++ packages/relay/src/lib.rs
@@ -1,5 +1,3 @@
 mod publish_contract;
 
-pub use publish_contract::{
-    ingestion_cli_error, ingestion_http_response, IngestionHttpResponse,
-};
+pub use publish_contract::{ingestion_cli_error, ingestion_http_response, IngestionHttpResponse};

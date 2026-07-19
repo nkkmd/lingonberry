27f8ac77f6cc1b0fcfaf958e47e20c36c57b8163
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-19 14:07:38 +0900

    refactor: export classified quarantine batch

--- packages/core/src/lib_v0_5.rs
+++ packages/core/src/lib_v0_5.rs
@@ -4,6 +4,7 @@ include!("lib_entry.rs");
 pub mod ingestion;
 pub mod classified_append;
 pub mod duplicate_conflict;
+pub mod quarantine_batch_classified;
 pub mod quarantine_promotion_classified;
 
 pub use classified_append::append_publish_request_classified;
@@ -15,4 +16,5 @@ pub use ingestion::{
     ingest_publish_request, publish_ingestion_result_json, PublishIngestionResult,
     PublishIngestionStatus, PUBLISH_INGESTION_CONTRACT_VERSION,
 };
+pub use quarantine_batch_classified::promote_quarantine_batch_classified;
 pub use quarantine_promotion_classified::promote_quarantine_record_classified;

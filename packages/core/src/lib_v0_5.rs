f8c080f2c3c5a13fd404cfb338573c1897978ef3
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-19 15:39:28 +0900

    feat: export basic query contract

--- packages/core/src/lib_v0_5.rs
+++ packages/core/src/lib_v0_5.rs
@@ -8,6 +8,7 @@ pub mod duplicate_conflict;
 pub mod object_retrieval;
 pub mod quarantine_batch_classified;
 pub mod quarantine_promotion_classified;
+pub mod query_contract;
 
 pub use archive_import_classified::import_archive_classified;
 pub use classified_append::append_publish_request_classified;
@@ -25,3 +26,7 @@ pub use object_retrieval::{
 };
 pub use quarantine_batch_classified::promote_quarantine_batch_classified;
 pub use quarantine_promotion_classified::promote_quarantine_record_classified;
+pub use query_contract::{
+    basic_query_result_json, execute_basic_query, BasicQueryResult, BasicQueryStatus,
+    BASIC_QUERY_CONTRACT_VERSION,
+};

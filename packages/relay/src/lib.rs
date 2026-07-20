9f7f12da7a44f671a8e307790aa02852974796c9
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-20 17:31:42 +0900

    Use effective view runtime v2

--- packages/relay/src/lib.rs
+++ packages/relay/src/lib.rs
@@ -1,10 +1,13 @@
+#[path = "effective_view_v2.rs"]
 mod effective_view;
 mod publish_contract;
 mod retrieval_contract;
 #[allow(clippy::manual_is_multiple_of)]
 mod transition_api;
 
-pub use effective_view::{effective_view_http_response, EffectiveViewHttpResponse};
+pub use effective_view::{
+    diagnostic_page_http_response, effective_view_http_response, EffectiveViewHttpResponse,
+};
 pub use publish_contract::{ingestion_cli_error, ingestion_http_response, IngestionHttpResponse};
 pub use retrieval_contract::{retrieval_http_response, RetrievalHttpResponse};
 pub use transition_api::{ingest_transition_request, TransitionHttpResponse};

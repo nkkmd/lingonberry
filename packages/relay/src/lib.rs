4b8ed02a1b07a2a7441de0cc078d42efa3a57a54
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-20 17:02:58 +0900

    feat(relay): export effective view API

--- packages/relay/src/lib.rs
+++ packages/relay/src/lib.rs
@@ -1,7 +1,9 @@
+mod effective_view;
 mod publish_contract;
 mod retrieval_contract;
 mod transition_api;
 
+pub use effective_view::{effective_view_http_response, EffectiveViewHttpResponse};
 pub use publish_contract::{ingestion_cli_error, ingestion_http_response, IngestionHttpResponse};
 pub use retrieval_contract::{retrieval_http_response, RetrievalHttpResponse};
 pub use transition_api::{ingest_transition_request, TransitionHttpResponse};

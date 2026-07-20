5dcb737ba2ef0922b9f8fa60b965e148d09ad248
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-20 17:37:20 +0900

    Export reevaluation worker

--- packages/relay/src/lib.rs
+++ packages/relay/src/lib.rs
@@ -1,6 +1,7 @@
 #[path = "effective_view_v2.rs"]
 mod effective_view;
 mod publish_contract;
+mod reevaluation_worker;
 mod retrieval_contract;
 #[allow(clippy::manual_is_multiple_of)]
 mod transition_api;
@@ -9,5 +10,9 @@ pub use effective_view::{
     diagnostic_page_http_response, effective_view_http_response, EffectiveViewHttpResponse,
 };
 pub use publish_contract::{ingestion_cli_error, ingestion_http_response, IngestionHttpResponse};
+pub use reevaluation_worker::{
+    process_reevaluation_queue, reconcile_reevaluation_queue, reevaluation_report_json,
+    ReevaluationReport,
+};
 pub use retrieval_contract::{retrieval_http_response, RetrievalHttpResponse};
 pub use transition_api::{ingest_transition_request, TransitionHttpResponse};
